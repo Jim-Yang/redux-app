2d2f3d1bf4030bda8187896d1e1baaba75581ecd
Jim Yang <jim.yang@solium.com>
2019-06-24 14:36:23 -0600

    Handling more actions

--- src/not-react/index.ts
+++ src/not-react/index.ts
@@ -1,7 +1,27 @@
-// Creating our own store
+type Action = {
+    type: ACTIONS,
+    todo: Todo
+}
 
-function createStore() {
-    let state: any
+type Todo = {
+    id: number,
+    name: string,
+    complete: boolean
+}
+
+type State = {
+    todos: Todo[]
+}
+
+enum ACTIONS {
+    ADD_TODO,
+    REMOVE_TODO,
+    TOGGLE_TODO,
+    ADD_GOAL
+}
+
+function createStore(reducer: any) {
+    let state: State
     let listeners: any = []
 
     const getState = () => state
@@ -13,14 +33,69 @@ function createStore() {
         }
     }
 
+    const dispatch = (action: Action) => {
+        // Call todos
+        state = reducer(state, action)
+        listeners.forEach((listener: any) => {listener()})
+        // Loop over listeners and invoke them
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
 
-const store = createStore()
+// Reducer Function
+function todos( state: State = {todos: []}, action: Action): State {
+    const actionTodo = action.todo
+    if(!actionTodo) {
+        return state
+    }
+    switch(action.type) {
+        case ACTIONS.ADD_TODO:
+            return Object.assign({}, {todos: state.todos.concat(actionTodo)}) // Return new object with todo
+        case ACTIONS.REMOVE_TODO:
+            return Object.assign({}, {todos: state.todos.filter( todo => (todo.id !== actionTodo.id))})
+        case ACTIONS.TOGGLE_TODO:
+            return Object.assign({}, {todos: state.todos.map(todo => {
+                return todo.id === actionTodo.id ? Object.assign({}, todo, { complete: !todo.complete }): todo
+            })})
+    }
+    
+    return state
+}
 
-store.subscribe(() => {
+const store = createStore(todos)
 
+const unsubscribe = store.subscribe(() => {
+    console.log(`The new state is: ${ JSON.stringify(store.getState())}`)
+})
+
+store.dispatch({
+    type: ACTIONS.ADD_TODO,
+    todo: {
+        id: 0,
+        name: 'Learn redux',
+        complete: false
+    }
+})
+
+store.dispatch({
+    type: ACTIONS.ADD_TODO,
+    todo: {
+        id: 1,
+        name: 'Learn react',
+        complete: false
+    }
+})
+
+store.dispatch({
+    type: ACTIONS.TOGGLE_TODO,
+    todo: {
+        id: 1,
+        name: 'Learn react',
+        complete: false
+    }
 })
