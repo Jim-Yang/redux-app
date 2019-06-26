5c405a04695fdde2d084df346925d90b249f3dfa
Jim Yang <jim.yang@solium.com>
2019-06-26 13:18:28 -0600

    Updates

---
+++ precache-manifest.feef63ffee57c9b4333e668fb124fb32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2bb25a3dbd2547596d1e96a97abad37",
+    "url": "/redux-app/index.html"
+  },
+  {
+    "revision": "b88d8d55fe3c1f36c752",
+    "url": "/redux-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "31b0084c2d462a3c60ea",
+    "url": "/redux-app/static/js/2.2ee725a2.chunk.js"
+  },
+  {
+    "revision": "b88d8d55fe3c1f36c752",
+    "url": "/redux-app/static/js/main.7935c0f1.chunk.js"
+  },
+  {
+    "revision": "40275f3f3af3ba99529a",
+    "url": "/redux-app/static/js/runtime~main.0b020989.js"
+  }
+]);
